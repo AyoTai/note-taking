f14b27c0f571e98e923ef66847fd5a5b1961d0ef
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-23 22:44:57 -0700

    push to save

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 
 
 // Routes
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,'./public/notes.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'./public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'./public/notes.html')));
 
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname,'./db/db.json'), (err, data) => {
@@ -48,19 +48,17 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
-app.delete('/api/notes/:id', (req,res) => {
+app.delete('/api/notes/:id', (req, res) => {
     let id = req.params;
+    console.log(id);
     fs.readFile(path.join(__dirname,'./db/db.json'), (err, data) => {
         if(err) {
-            console.log(`error here`)
+            console.log('Error')
             res.status(500);
-            
         }
-        let savedNotes = JSON.parse(data);
-        let editedNotes = savedNotes.filter( (data) => {
-            console.log(`NOTE ${data}`)
-            return data.id !== id;
-        });
+        console.log(JSON.parse(data));
+        data.deleteById(id);
+    
     })
 });
 
